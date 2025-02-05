195d48a017354f770f92eea3b6bd14e4cec58e1e
Alex.Vale <alex.vale@enable.com>
2025-02-05 14:34:28 +0000

    idk

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
     plugins: [svelte()],
     assetsInclude: ['**/*.png', '**/*.svg', '**/*.ico'],
-    base: '/',
+    base: '/PokemonMemoryGame',
     build: {
         outDir: './docs',
     },
