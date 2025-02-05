54368be78ba264b3e51ab5e15942bc456c122bcf
Alex.Vale <alex.vale@enable.com>
2025-02-05 14:16:40 +0000

    Try different hosting strat

--- pokemon-memory-game/vite.config.ts
+++ pokemon-memory-game/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  base: '/',
-  build: {
-      outDir: '../',
-  },
+    plugins: [svelte()],
+    assetsInclude: ['**/*.png', '**/*.svg', '**/*.ico'],
+    base: '/',
+    build: {
+        outDir: './docs',
+    },
 })
